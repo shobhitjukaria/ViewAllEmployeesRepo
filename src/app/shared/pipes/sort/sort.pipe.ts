e9fb90f4ccb9bd5124aa8260712fc03ec7385f8e
Shobhit Jukaria <shobhitjukaria@gmail.com>
2020-08-26 00:59:53 +0530

    Implemented Sort Filter

--- src/app/shared/pipes/sort/sort.pipe.ts
+++ src/app/shared/pipes/sort/sort.pipe.ts
@@ -1,12 +1,34 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'sort'
+  name: 'sort',
+  pure: true
 })
 export class SortPipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return null;
+  transform(list: any[], column:string , count:number): any[] {
+    if(count%2==0){
+    if(list !== undefined){
+    let sortedArray = list.sort((a,b) => {
+       if(a[column]>b[column]){return 1;}
+       if(a[column]<b[column]){return -1;}
+        return 0;
+    });
+    column="";
+    return sortedArray;
+  }}
+  else{
+    if(list !== undefined){
+    let sortedArray = list.sort((a,b) => {
+       if(a[column]>b[column]){return -1;}
+       if(a[column]<b[column]){return 1;}
+        return 0;
+    });
+    column="";
+    return sortedArray;
   }
+
+  }
+}
 
 }
